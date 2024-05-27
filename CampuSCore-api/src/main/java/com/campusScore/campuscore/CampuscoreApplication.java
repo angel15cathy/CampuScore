8f07ac9524124ed42b97c936096079d00e2fcde5
Cathy <evandjeewanecathyangella15@gmail.com>
2024-05-27 13:31:30 +0100

    initialisation des different dossier model, repository, service, controller

---
+++ CampuSCore-api/src/main/java/com/campusScore/campuscore/CampuscoreApplication.java
@@ -0,0 +1,13 @@
+package com.campusScore.campuscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscoreApplication.class, args);
+	}
+
+}
---
+++ CampuSCore-api/src/test/java/com/campusScore/campuscore/CampuscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusScore.campuscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
